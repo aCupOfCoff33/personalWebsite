4aa01a97e3f40651f06ff90396d7af32a8211660
Aaryan Joharapurkar <aaryanj@outlook.com>
2024-12-21 17:14:13 -0500

    changed the bubble of the work experience section

--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -3,31 +3,52 @@ import { HiExternalLink } from "react-icons/hi";
 
 const WorkExperience = ({ date, title, company, description, tags, link, icon }) => {
   return (
-    <div className="flex flex-col md:flex-row mb-8">
-      {/* Date Section */}
+    <div className="flex flex-col md:flex-row mb-8 items-start">
+      {/* Date */}
       <div className="md:w-1/4 mb-2 md:mb-0 text-gray-400 text-sm md:text-right pr-4">
         {date}
       </div>
-      {/* Main Content */}
+
+      {/* Details Section */}
       <div className="md:w-3/4">
-        <h3 className="text-white text-lg font-bold flex items-center">
-          {title}{" "}
-          <img
-            src={icon}
-            alt={`${company} logo`}
-            className="h-6 w-6 rounded-full ml-2 mr-2 transition-transform transform hover:scale-105"
-          />
+        {/* Title and Company */}
+        <div className="flex items-center">
+          <span className="text-white font-bold text-lg">
+            {title} <span className="text-gray-400">@</span>
+          </span>
+          {/* Icon */}
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-300 transition-all ml-1 flex items-center"
+            className="mx-2 flex-shrink-0 transform transition-transform duration-300"
           >
-            @{company}
-            <HiExternalLink className="inline ml-1 hover:translate-y-[-2px]" />
+            <img
+              src={icon}
+              alt={company}
+              className="w-8 h-8 rounded-lg hover:rounded-md hover:scale-105 transition-all duration-300"
+            />
           </a>
-        </h3>
+          {/* Company Name */}
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center group text-white font-bold text-lg transition-all duration-300"
+          >
+            <span className="group-hover:text-gray-300 underline">
+              {company}
+            </span>
+            <HiExternalLink
+              className="ml-1 text-gray-400 group-hover:text-gray-300 group-hover:translate-y-[-2px] group-hover:translate-x-[2px] transition-transform duration-300"
+            />
+          </a>
+        </div>
+
+        {/* Description */}
         <p className="text-gray-300 text-sm mt-2">{description}</p>
+
+        {/* Tags */}
         <div className="mt-2 flex flex-wrap gap-2">
           {tags.map((tag, index) => (
             <span
