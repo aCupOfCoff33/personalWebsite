7940b1cbab26760cdf9af9a526012208a4ca860b
Aaryan <aaryanj@outlook.com>
2025-10-07 17:18:06 -0400

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,18 +5,19 @@ const AboutMe = () => {
     <div className="w-full text-white text-lg sm:text-xl lg:text-2xl font-['DM Sans'] space-y-3 sm:py-16 sm:space-y-4 lg:space-y-6 px-4 sm:px-6 lg:px-0">
       <p>hey there! 👋</p>
       <p>
-        i’m a software engineer 🖥️ , ui/ux enthusiast 🎨 , and consultant 🤝 located in
-        toronto, canada. 🇨🇦
+        i’m a software engineer 🖥️, ui/ux enthusiast 🎨, and consultant 🤝 based in
+        toronto, canada. 🇨🇦 i’m currently pursuing a dual degree in software
+        engineering and business at western university & ivey business school. 🎓
       </p>
       <p>
-        i’m passionate about tackling challenges across diverse domains—whether
-        it’s crafting intuitive designs ✏️ , building intelligent systems 🤖 ,
-        navigating fintech innovations 💸 , or delivering strategic solutions as a
-        consultant. 📊
+        i’m passionate about taking complex, unstructured datasets and uncovering
+        meaningful relationships within them. turning that complexity into
+        intuitive, story-driven insights that people can act on is really cool! whether it’s in
+        private equity, fintech, or any data-heavy space, i love building systems
+        that make information feel obvious, visual, and actionable. ⚡
       </p>
       <p>
-        this space is my curated playground , where i share projects and ideas
-        that inspire me. ✨
+        this space is where i share projects that bring those ideas to life. ✨
       </p>
     </div>
   );
